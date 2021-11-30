5404d2a05363e3d31a624a3b67ae9d52468d85cf
DaLei Sun <dalei@DaLeis-MacBook-Pro.local>
2021-11-30 23:00:16 +0800

    test

--- tacocloud-messaging-jms/src/main/java/tacos/messaging/JmsOrderMessagingService.java
+++ tacocloud-messaging-jms/src/main/java/tacos/messaging/JmsOrderMessagingService.java
@@ -2,25 +2,27 @@ package tacos.messaging;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 import tacos.Order;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
+import javax.jms.Destination;
 
 @Service
 public class JmsOrderMessagingService implements OrderMessagingService {
+
     private JmsTemplate jms;
 
+    private Destination orderQueue;
+
     @Autowired
-    public JmsOrderMessagingService(JmsTemplate jms) {
+    public JmsOrderMessagingService(JmsTemplate jms, Destination orderQueue) {
         this.jms = jms;
+        this.orderQueue = orderQueue;
     }
 
     @Override
     public void sendOrder(Order order) {
-        jms.send(session -> session.createObjectMessage(order));
+        //jms.send(orderQueue, session -> session.createObjectMessage(order));
+        jms.convertAndSend(orderQueue, order);
     }
 }
--- tacocloud-messaging-jms/src/main/java/tacos/messaging/MessagingConfig.java
+++ tacocloud-messaging-jms/src/main/java/tacos/messaging/MessagingConfig.java
@@ -1,10 +1,15 @@
 package tacos.messaging;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
+import org.apache.commons.collections.map.AbstractMapDecorator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import tacos.Order;
 
 import javax.jms.Destination;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class MessagingConfig {
@@ -12,5 +17,18 @@ public class MessagingConfig {
     @Bean
     public Destination orderQueue() {
         return new ActiveMQQueue("tacocloud.order.queue");
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter messageConverter() {
+        MappingJackson2MessageConverter messageConverter =
+                new MappingJackson2MessageConverter();
+        messageConverter.setTypeIdPropertyName("_typeId");
+
+        Map<String, Class<?>> typeIdMappings = new HashMap<>();
+        typeIdMappings.put("order", Order.class);
+        messageConverter.setTypeIdMappings(typeIdMappings);
+        
+        return messageConverter;
     }
 }
