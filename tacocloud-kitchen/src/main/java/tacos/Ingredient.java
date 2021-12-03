eff40224ddad795d95c7ef4987c21046656632eb
DaLei Sun <dalei.sun@infor.com>
2021-12-03 14:46:01 +0800

    default constructor for ingredient

--- tacocloud-kitchen/src/main/java/tacos/Ingredient.java
+++ tacocloud-kitchen/src/main/java/tacos/Ingredient.java
@@ -1,13 +1,15 @@
 package tacos;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Ingredient {
 
-    private final String name;
+    private String name;
 
-    private final Type type;
+    private Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
