3e61e668a040884f1070b0e2ecd9b0acf7ada9d5
DaLei Sun <dalei@DaLeis-MacBook-Pro.local>
2021-12-06 11:19:24 +0800

    rabbitmq

--- tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitOrderReceiver.java
+++ tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/rabbit/RabbitOrderReceiver.java
@@ -1,10 +1,9 @@
 package tacos.kitchen.messaging.rabbit;
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import tacos.Order;
 import tacos.kitchen.OrderReceiver;
@@ -15,19 +14,14 @@ public class RabbitOrderReceiver implements OrderReceiver {
 
     private RabbitTemplate rabbitTemplate;
 
-    private MessageConverter messageConverter;
-
     @Autowired
     public RabbitOrderReceiver(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
-        this.messageConverter = rabbitTemplate.getMessageConverter();
     }
     
     @Override
     public Order receiveOrder() {
-        Message message = rabbitTemplate.receive("tacocloud.orders");
-        return message != null ?
-                (Order) messageConverter.fromMessage(message)
-                : null;
+        return rabbitTemplate.receiveAndConvert("tacocloud.orders", new ParameterizedTypeReference<>() {
+        });
     }
 }
