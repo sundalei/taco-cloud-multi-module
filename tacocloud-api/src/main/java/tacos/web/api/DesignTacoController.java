0f59ae8bc070a87f53ae574e514108348647c411
DaLei Sun <dalei@DaLeis-MacBook-Pro.local>
2021-12-30 22:20:57 +0800

    web flux

--- tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
+++ tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -1,28 +1,20 @@
 package tacos.web.api;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.WebProperties;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.EntityLinks;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import reactor.core.publisher.Flux;
 import tacos.Taco;
 import tacos.data.TacoRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/design", produces = "application/json")
 @CrossOrigin(origins = "*")
-@Slf4j
 public class DesignTacoController {
 
     private final TacoRepository tacoRepo;
@@ -36,16 +28,9 @@ public class DesignTacoController {
     }
 
     @GetMapping("/recent")
-    public CollectionModel<TacoResource> recentTacos() {
-        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
-
-        List<Taco> tacos = tacoRepo.findAll(page).getContent();
-        CollectionModel<TacoResource> recentResources = new TacoResourceAssembler().toCollectionModel(tacos);
-
-        recentResources.add(WebMvcLinkBuilder.linkTo(DesignTacoController.class)
-                .slash("recent")
-                .withRel("recents"));
-        return recentResources;
+    public Flux<Taco> recentTacos() {
+        return Flux.fromIterable(tacoRepo.findAll()).take(12);
+        
     }
 
     @GetMapping("/{id}")
